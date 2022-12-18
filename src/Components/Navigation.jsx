5fc3fae25c141a5ffb4f19c688ca53595311076e
apointproven <apointproven@gmail.com>
2022-12-18 03:45:29 -0500

    Not sure just yet

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -8,8 +8,6 @@ const navbar = () => {
                 <img src={logo} alt="logo" className="px-16 cursor-pointer h-auto" />
                 <p className="hidden text-accentPink absolute text-2xl font-bold left-40">/</p>
                 <p className="hidden text-dmText dark:text-lmText absolute text-2xl font-medium left-44">APointProven</p>
-
-
             </div>
         </div>
     )
